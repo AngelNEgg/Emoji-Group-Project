a9db725f96fa2c618bc70f7f65c9376d975ac21b
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-09 12:32:20 -0500

    Update projectFinal.py

--- projectFinal.py
+++ projectFinal.py
@@ -6,7 +6,7 @@ scale = 0.25
 
 def draw(canvas):
   
-# face 1
+# face 1 (Angel)
   canvas.draw_circle((xmax/2,ymax/2), 100, 10, "#fcb503", "#fcd703")
   canvas.draw_circle((xmax/2-25,ymax/2-25), 5, 10, "Black")
   canvas.draw_circle((xmax/2+25,ymax/2-25), 5, 10, "Black")
