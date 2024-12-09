975f990b7364616d6ecbc4c9159eb993840cd3ff
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-09 12:42:54 -0500

    Update projectFinal.py

--- projectFinal.py
+++ projectFinal.py
@@ -20,11 +20,11 @@ def draw(canvas):
   canvas.draw_circle(((width/2) -30,(height/2) -30), 10, 10, "white")
 
 # face 3 (Nafisa)
-    canvas.draw_circle((width/2.8, height/3), width/3 - 50, 10, "#ADD8E6", "#ADD8E6")
-    canvas.draw_circle((width/4,height/3), width/20 + 5, 10, "Black", "Black")
-    canvas.draw_circle((width/2 + 10, height/3), width/20 + 5, 10, "Black", "Black")
-    canvas.draw_polygon([(width/4 - 15, height/2 + 30), (width/4 - 15, height/3 + 30), (width/4 - 15, height - 20) ], 35, "Blue", "Blue")
-    canvas.draw_polygon([(width/2 + 5, height/3 + 30), (width/2 + 5, height/3 + 30), (width/2 + 5, height - 20) ], 35, "Blue", "Blue")
+  canvas.draw_circle((width/2.8, height/3), width/3 - 50, 10, "#ADD8E6", "#ADD8E6")
+  canvas.draw_circle((width/4,height/3), width/20 + 5, 10, "Black", "Black")
+  canvas.draw_circle((width/2 + 10, height/3), width/20 + 5, 10, "Black", "Black")
+  canvas.draw_polygon([(width/4 - 15, height/2 + 30), (width/4 - 15, height/3 + 30), (width/4 - 15, height - 20) ], 35, "Blue", "Blue")
+  canvas.draw_polygon([(width/2 + 5, height/3 + 30), (width/2 + 5, height/3 + 30), (width/2 + 5, height - 20) ], 35, "Blue", "Blue")
 
 # face 4
 
