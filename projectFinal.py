260a5556f545cd2d370ff8c6303fc5c046fe2ccc
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-09 12:32:09 -0500

    Update projectFinal.py

--- projectFinal.py
+++ projectFinal.py
@@ -2,18 +2,24 @@ import simplegui
 
 height = 600
 width = 600
+scale = 0.25
 
+def draw(canvas):
+  
 # face 1
-
+  canvas.draw_circle((xmax/2,ymax/2), 100, 10, "#fcb503", "#fcd703")
+  canvas.draw_circle((xmax/2-25,ymax/2-25), 5, 10, "Black")
+  canvas.draw_circle((xmax/2+25,ymax/2-25), 5, 10, "Black")
+  canvas.draw_line((xmax/2-50,ymax/2+25),(xmax/2+50,ymax/2+25), 5, "brown")
+  
 # face 2
-
+  
 # face 3
-
+  
 # face 4
 
-# draw faces
 
 # frame setup
-
 frame = simplegui.create_frame("Emojis", width, height)
+frame.set_draw_handler(draw)
 frame.start()
